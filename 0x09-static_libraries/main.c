0c2cf68188c0cc062209b7f0f17c12b188d719d5
Mudeya2023 <mudeyamichael@gmail.com>
2023-04-08 03:54:33 -0700

    my tasks

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-int main(void)
+int main _puts(
+"\ \"\n\t-
+)
 {
-_puts("At the end of the day,my goal was to be the best hacker Kevin Mitnick");
+"At the end of the day, my goal was to be the best hacker" - Kevin Mitnick
 return (0);
 }
