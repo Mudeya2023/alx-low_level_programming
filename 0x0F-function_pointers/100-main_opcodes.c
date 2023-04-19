922f71d073c90e7883e3a42cf5c0fa6544b0fd51
Mudeya2023 <mudeyamichael@gmail.com>
2023-04-19 05:08:49 -0700

    Mike the software engineering company

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,8 +4,7 @@
 /**
  * main - check the code for ALX Afrcia SE students.
  * @argc: argument count.
- * @argv: argument vector.
- 
+ * @argv: argument vector. 
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
