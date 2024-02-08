ceb7c910f4620a780a316bee7db07f0cdfedb34c
Mudeya2023 <mudeyamichael@gmail.com>
2024-02-08 13:29:27 -0800

    mike

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,16 @@
 unsigned int binary_to_uint(const char *b)
 
 {
-	int i;
-	unsigned int dec_val = 0;
+int i;
+unsigned int dec_val = 0;
 
-	if (!b)
-	return (0);
-	for (i = 0; b[i]; i++)
-	{
-	if (b[i] < '0' || b[i] > '1')
-	return (0);
-	dec_val = 2 * dec_val + (b[i] - '0');
-	}
-	return (dec_val);
+#if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+dec_val = 2 * dec_val + (b[i] - '0');
 }
-	
+return (dec_val);
+}	
